851ff6ac8c91e2dfcb53f926bef921e9b712ce19
Hiep <np71379@gmail.com>
2019-09-20 16:14:18 +0700

    First commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    ///
+    /// 1 - The speed of the ship
+    ///
+    public Vector2 speed = new Vector2(50, 50);
+    // 2 - Store the movement
+    private Vector2 movement;
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        // 4 - Movement per direction
+        movement = new Vector2(
+        speed.x * inputX,
+        speed.y * inputY);
+    }
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        //rigidbody2D.velocity = movement;
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
